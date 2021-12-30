a67b4acf05e729ef765fec4e5d54d73801c71c23
Jan Loeffel <jan.loeffel@spoonsoft.de>
2021-12-30 19:47:29 +0100

    Initial Commit

---
+++ SpoonFW/SpoonFW.h
@@ -0,0 +1,18 @@
+//
+//  SpoonFW.h
+//  SpoonFW
+//
+//  Created by Jan Löffel on 30.12.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpoonFW.
+FOUNDATION_EXPORT double SpoonFWVersionNumber;
+
+//! Project version string for SpoonFW.
+FOUNDATION_EXPORT const unsigned char SpoonFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpoonFW/PublicHeader.h>
+
+
